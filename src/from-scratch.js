8e82dedaca6d9ec4b14849f2a8e1a6b27398e983
ZaneC04 <zenithzsc@gmail.com>
2025-10-14 18:19:25 -0400

    passed all tests

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,48 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if (isFront === true) {
+    arr.unshift(value)
+  } else {
+    arr.push(value)
+  }
 };
 
-const reverseString = () => {
+const reverseString = (string) => {
+  str = string.split('');
+  str.reverse();
+  return str.join("");
 };
 
-const newArrayFullOf = () => {
+const newArrayFullOf = (value, numOfValue) => {
+  let arr = new Array(numOfValue)
+  return arr.fill(value, 0, numOfValue)
 };
 
-const insertIntoMiddle = () => {
+const insertIntoMiddle = (arr, value) => {
+  arr.splice(Math.floor(arr.length / 2), 0, value)
 };
 
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr) => {
+  arr.splice(Math.floor(arr.length / 2), 1)
 };
 
-const isRightIndex = () => {
+const isRightIndex = (arr, value, index) => {
+  return arr[index] === value ? true : false
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(Math.floor(arr[i]))
+  }
+  return newArr
 };
 
-const getAllYCoordinates = () => {
+const getAllYCoordinates = (arrOfCoords) => {
+  let newArr = [];
+  for (let i = 0; i < arrOfCoords.length; i++) {
+    newArr.push(arrOfCoords[i][1])
+  }
+  return newArr
 };
 
 module.exports = {
